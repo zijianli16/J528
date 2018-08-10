29e93659d208a61eb1beb3cd81f2e08fc66db588
Alex Li <42188330+zijianli16@users.noreply.github.com>
2018-08-09 21:02:34 -0700

    ALex uploads a template to the server

---
+++ web design/js/student.js
@@ -0,0 +1,11 @@
+/* student.js 
+   Author: 
+   Date: 
+*/
+
+
+$(document).ready(function(){ // begin document.ready block
+
+	/* your javascript and jquery  code here */
+
+}); //end document.ready block
